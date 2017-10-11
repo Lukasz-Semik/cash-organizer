cd76c1613df5b2db3bddd2e6dafc63770b6fe232
djpluki <djpluki@gmail.com>
2017-10-11 23:21:06 +0200

    simple redirection if not logged in

--- app/src/App.js
+++ app/src/App.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import { firebaseApp } from '../firebase';
 
 class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      user: firebaseApp.auth().currentUser
+    }
+  }
   logOut(){
     const history = this.props.history;
     firebaseApp.auth().signOut()
     .then(()=>{history.push('/')})
     .catch(error=>console.log(error));
   }
+  componentDidMount(){
+    if(!this.state.user){
+      this.props.history.push('/');
+    }
+  }
   render(){
+    this.state.user && console.log(this.state.user.email);
     return(
       <div>
         <h1>Hello from App</h1>
